cefa7c9a1be06ef5882bad41c1dc79a8c3f1ea5e
verteramofederico <verteramo.federico@gmail.com>
2021-12-17 15:33:50 -0300

    changes in usercontroller responses

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ require('dotenv').config()
 
 const express = require('express');
 const path = require('path');
+var cors = require('cors')
 
 const routes = require('./Routes/IndexRouter')
 
 const app = express();
 
 app.use(express.json())
+app.use(cors())
 
 app.use(routes)
 
--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -58,7 +58,7 @@ module.exports = {
             { expiresIn: 86400 },
             (error, token) => {
                 if (error) {throw error};
-                return res.status(201).json({ token });
+                return res.status(201).json({ token, name: user.name });
             }
         )
     },
@@ -85,7 +85,7 @@ module.exports = {
             { expiresIn: 86400 },
             (error, token) => {
                 if (error) throw error;
-                return res.status(201).json({ token });
+                return res.status(201).json({ token, name: user.name });
             }
         )
     }
